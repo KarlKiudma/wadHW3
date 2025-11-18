6d27112443640ea8dbae2844e1dbe6e96b5b0a16
renedapon <dapon@ut.ee>
2025-11-18 16:10:55 +0200

    Header and footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="footer">
+        <p>Web Application Development 2025 - Group N <br>
+        All rights reserved.</p>
+    </footer>
+</template>
+
+<script>
+export default{
+    name: "FooterComponent",
+};
+</script>
+
+<style>
+.footer{
+    width: 100%;
+    background-color: lightslategray;
+    padding: 15px 25px;
+    text-align: center;
+    box-sizing: border-box;
+    font-size: small;
+    color: black;
+    font-family: 'Inter', sans-serif;
+
+}
+</style>
